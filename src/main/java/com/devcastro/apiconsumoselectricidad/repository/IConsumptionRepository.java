0b8b087cb4c0336fbeb45907f9d0eb8a26c5b1be
Duvan Castro <duvancastro1026@gmail.com>
2023-04-24 04:24:15 -0500

    add file

--- src/main/java/com/devcastro/apiconsumoselectricidad/repository/IConsumptionRepository.java
+++ src/main/java/com/devcastro/apiconsumoselectricidad/repository/IConsumptionRepository.java
@@ -1,4 +1,10 @@
 package com.devcastro.apiconsumoselectricidad.repository;
 
-public interface IConsumptionRepository {
+
+import com.devcastro.apiconsumoselectricidad.entities.ElectricalConsumption;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IConsumptionRepository extends JpaRepository<ElectricalConsumption, Integer> {
 }
