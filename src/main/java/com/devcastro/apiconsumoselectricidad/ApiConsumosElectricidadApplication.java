ce5c71d3955428b4c6f418cf9ffe718744369175
Duvan Castro <duvancastro1026@gmail.com>
2023-04-24 03:46:24 -0500

    add files all project

---
+++ src/main/java/com/devcastro/apiconsumoselectricidad/ApiConsumosElectricidadApplication.java
@@ -0,0 +1,13 @@
+package com.devcastro.apiconsumoselectricidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsumosElectricidadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConsumosElectricidadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devcastro/apiconsumoselectricidad/ApiConsumosElectricidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcastro.apiconsumoselectricidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsumosElectricidadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
